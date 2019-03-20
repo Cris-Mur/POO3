35b284eb40efda3f56e59d67cc1930e2db4a3155
Cris-Mur <yomy98@gmail.com>
2019-03-20 12:02:01 -0500

    add project folder Talleres/06-03-19

---
+++ Talleres/06-03-19/Project10/Project10/Classes.cpp
@@ -0,0 +1,193 @@
+#include "Classes.h"
+#include <iostream>
+
+Lista::Lista() {
+	dir = -1;
+	size = -1;
+	control = false;
+}
+
+Lista::~Lista() {
+
+}
+
+void Lista::set_size(int pm) {
+	size = pm;
+}
+
+int Lista::create(int pm) {
+	set_size(pm);
+	arr = new int[size];
+	dir = 0;
+	return dir;
+}
+
+bool Lista::cleanTest() {
+	if (dir == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+void Lista::add(int helper_dir, int dato) {
+	int j;
+	if (dir < size) {
+		control = true;
+		if (helper_dir <= dir + 1) {
+			j = dir;
+			while (j >= helper_dir) {
+				arr[j + 1] = arr[j];
+				j = j - 1;
+			}
+			arr[helper_dir] = dato;
+			dir = dir + 1;
+		}
+		else {
+			control = false;
+		}
+	}
+	else {
+		control = false;
+	}
+
+}
+
+int Lista::view(int helper_dir, int dato) {
+	if (Lista::cleanTest()) {
+		control = false;
+		dato = arr[0];
+		return dato;
+	}
+	else {
+		control = true;
+		dato = arr[helper_dir];
+		return dato;
+	}
+}
+
+void Lista::eliminate(int helper_dir, int dato) {
+	int cont = helper_dir;
+	if (cont <= dir) {
+
+		while (cont <= dir) {
+			arr[cont] = arr[cont + 1];
+			cont++;
+		}
+		arr[dir] = NULL;
+		control = true;
+		dir = dir - 1;
+
+	}
+	else {
+		control = false;
+		dato = arr[0];
+	}
+}
+
+void Lista::destroy(int helper_dir, int dato) {
+	helper_dir = 1;
+	while (dir > 0) {
+		eliminate(helper_dir, dato);
+	}
+	delete arr;
+	Lista();
+	
+}
+
+
+
+Pila::Pila() {
+	base = -1;
+	top = -1;
+	control = false;
+}
+
+Pila::~Pila() {
+
+}
+
+void Pila::set_sizePila(int pm) {
+	size = pm;
+}
+
+int Pila::create(int pm) {
+	set_sizePila(pm);
+	pila = new int[size];
+	base = 0;
+	top = 0;
+	return top;
+}
+
+bool Pila::cleanTest() {
+	if (base == 0 && top == 0) {
+		return true;
+	}
+	else {
+		return false;
+	}
+}
+
+
+void Pila::add(int helper_top, int dato) {
+	int j;
+	if (top < size) {
+		control = true;
+		if (helper_top <= top + 1) {
+			j = top;
+			while (j >= helper_top) {
+
+				pila[j + 1] = pila[j];
+				j = j - 1;
+			}
+			pila[helper_top] = dato;
+			top = top + 1;
+		}
+		else {
+			control = false;
+		}
+	}
+	else {
+		control = false;
+	}
+}
+
+int Pila::view(int helper_top, int dato) {
+	if (cleanTest()) {
+		control = false;
+		dato = pila[base];
+		return dato;
+	}
+	else {
+		control = true;
+		dato = pila[helper_top];
+		return dato;
+	}
+}
+
+void Pila::eliminate(int helper_top, int dato) {
+	int cont = helper_top;
+	if (cont <= top) {
+		while (cont <= top) {
+			pila[cont] = pila[cont + 1];
+			cont++;
+		}
+		pila[top] = pila[base];
+		control = true;
+		top = top - 1;
+	}
+	else {
+		control = false;
+		dato = pila[base];
+	}
+}
+
+void Pila::destroy(int helper_top, int dato) {
+	helper_top = 1;
+	while (top > 0) {
+		eliminate(helper_top, dato);
+	}
+	Pila();
+	delete pila;
+}
---
+++ Talleres/06-03-19/Project10/Project10/lista.h
@@ -0,0 +1,73 @@
+#pragma once
+op = 0;
+int helper_dir;
+Lista lista;
+while (op != 7) {
+	menuLista();
+	std::cin >> op;
+	switch (op) {
+	case 1:
+		std::cout << "Ingrese tamaño de la lista." << '\n'
+			<< "Tamaño: ";
+		std::cin >> pm;
+		lista.create(pm);
+		std::cout << "lista de tamaño: " << lista.size << '\n'
+			<< "Creada satisfactoriamente" << '\n';
+		break;
+	case 2:
+		if (lista.size == -1) {
+			std::cout << "la lista no existe." << '\n';
+		}
+		else {
+			if (lista.cleanTest()) {
+				std::cout << "Pila creada y vacia." << '\n';
+			}
+			else {
+				std::cout << "la pila no existe o no esta vacia." << '\n';
+			}
+
+		}
+		break;
+	case 3:
+		std::cout << "Direccion y dato a ingresar en la lista." << '\n'
+			<< "Direccion: ";
+		std::cin >> helper_dir;
+		std::cout << "Dato: ";
+		std::cin >> dato;
+		lista.add(helper_dir, dato);
+		break;
+	case 4:
+		std::cout << "Direccion de la lista que desea ver." << '\n'
+			<< "Direccion: ";
+		std::cin >> helper_dir;
+		std::cout << "lista [ " << helper_dir << " ]: "
+			<< lista.view(helper_dir, dato) << '\n';
+		break;
+	case 5:
+		std::cout << "ultima posicion ingresada de la lista: "
+			<< lista.dir << '\n';
+		std::cout << "Direccion a eliminar de la lista." << '\n'
+			<< "Direccion: ";
+		std::cin >> helper_dir;
+		lista.eliminate(helper_dir, dato);
+		std::cout << "elemento eliminado." << '\n';
+		std::cout << "ACTUALIZADO lista [ " << helper_dir << " ]: "
+			<< lista.view(helper_dir, dato) << '\n';
+		break;
+	case 6:
+		std::cout << "ATENCION:" << '\n'
+			<< "Va a destruir la lista está seguro?" << '\n'
+			<< "Opcion s/n :  ";
+		std::cin >> o;
+		switch (o) {
+		case 115:
+			lista.destroy(helper_dir, dato);
+			std::cout << "la lista ha sido destruida..." << '\n';
+			break;
+		case 110:
+			break;
+		}
+		break;
+
+	}
+}
